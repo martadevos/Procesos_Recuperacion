aca1cea5992bacb21f101f1224de924a2ffe84f2
mdevos <mdevos@107-07>
2023-04-18 13:43:29 +0200

    18/04/2023

--- Recuperacion_Unidad_3/src/ejercicios_conexion_tcp/ejercicio1/Servidor.java
+++ Recuperacion_Unidad_3/src/ejercicios_conexion_tcp/ejercicio1/Servidor.java
@@ -1,4 +1,41 @@
 package ejercicios_conexion_tcp.ejercicio1;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Servidor {
+    public static void main(String[] args) {
+        try {
+            System.out.println("SERVER:\nAbriendo conexión...");
+            ServerSocket socketServer = new ServerSocket(50000);
+
+            while (true){
+                System.out.println("SERVER:\nEsperando peticiones...");
+                Socket socketClient = socketServer.accept();
+
+                System.out.println("SERVER:\nAbriendo flujos de E/S...");
+                InputStream is = socketClient.getInputStream();
+                OutputStream os = socketClient.getOutputStream();
+
+                System.out.println("SERVER:\nLeyendo mensaje del cliente...");
+                System.out.println("Mensaje cliente: " + is.read());
+
+                System.out.println("SERVER:\nEnviando mensaje al cliente...");
+                os.write(7);
+
+                System.out.println("SERVER:\nCerrando flujos de E/S...");
+                is.close();
+                os.close();
+
+                socketClient.close();
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error al crear el socket");
+            e.printStackTrace();
+        }
+    }
 }
