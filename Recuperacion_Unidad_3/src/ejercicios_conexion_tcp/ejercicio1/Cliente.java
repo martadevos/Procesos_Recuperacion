c6a595d49c54b25de3b4e88914bcf5cb477132e1
mdevos <mdevos@107-07>
2023-04-20 11:45:37 +0200

    20/04/2023

--- Recuperacion_Unidad_3/src/ejercicios_conexion_tcp/ejercicio1/Cliente.java
+++ Recuperacion_Unidad_3/src/ejercicios_conexion_tcp/ejercicio1/Cliente.java
@@ -1,7 +1,59 @@
 package ejercicios_conexion_tcp.ejercicio1;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class Cliente {
     public static void main(String[] args) {
+        try{
+            System.out.println("CLIENTE\nCreando socket...\n");
+            InetAddress direccion = InetAddress.getLocalHost(); //Coge el localhost
+            Socket socketClient = new Socket(direccion, 2500); //Crea el socket con el localhost y el puerto 2500
 
+            System.out.println("CLIENTE\nAbriendo flujos de E/S...\n");
+            OutputStream os = socketClient.getOutputStream(); //Abre flujo esctritura
+            InputStream is = socketClient.getInputStream(); //Abre flujo lectura
+
+            System.out.println("CLIENTE\nEnviando mensaje al servidor...");
+            os.write(pedirNumACliente()); //Llama a la función para pedir un númeroy lo envía al servidor
+
+            System.out.println("CLIENTE\nLeyendo mensaje del servidor...\n");
+            System.out.println("MENSAJE DEL SERVIDOR\n" + is.read() + "\n"); //Recibe y muestra por consola el mensaje del servidor
+
+            System.out.println("CLIENTE\nCerrando flujos de E/S...\n");
+            os.close(); //Cierra flujo lectura
+            is.close(); //Cierra flujo escritura
+
+            socketClient.close(); //Cierra la conexión
+        } catch (UnknownHostException e) {
+            System.out.println("Error, no se encuentra el host");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Error con la conexión");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Función que pide un número por consola, lo escanea, comprueba si es positivo y
+     * sigue pidiendo y comprobando hasta que introduce uno positivo y lo devuelve
+     *
+     * @return Entero introducido por consola
+     **/
+    private static int pedirNumACliente(){
+        Scanner s = new Scanner(System.in);
+        int num;
+
+        do {
+            System.out.println("Introduzca un número entero positivo");
+            num = s.nextInt();
+            if (num < 0) System.out.println("Error, el número no puede ser negativo");;
+        }while (num < 0);
+        return num;
     }
 }
--- Recuperacion_Unidad_3/src/ejercicios_conexion_tcp/ejercicio1/Servidor.java
+++ Recuperacion_Unidad_3/src/ejercicios_conexion_tcp/ejercicio1/Servidor.java
@@ -5,30 +5,34 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Servidor {
     public static void main(String[] args) {
+        int numCliente;
         try {
-            System.out.println("SERVER:\nAbriendo conexión...");
-            ServerSocket socketServer = new ServerSocket(50000);
+            System.out.println("SERVER:\nAbriendo conexión...\n");
+            ServerSocket socketServer = new ServerSocket(2500); //crea el socket con el puerto 2500
 
             while (true){
-                System.out.println("SERVER:\nEsperando peticiones...");
-                Socket socketClient = socketServer.accept();
+                System.out.println("SERVER:\nEsperando peticiones...\n");
+                Socket socketClient = socketServer.accept(); //Espera a que haya una petición del cliente y la acepta cunado llega
 
-                System.out.println("SERVER:\nAbriendo flujos de E/S...");
-                InputStream is = socketClient.getInputStream();
-                OutputStream os = socketClient.getOutputStream();
+                System.out.println("SERVER:\nAbriendo flujos de E/S...\n");
+                InputStream is = socketClient.getInputStream(); //Abre flujo de lectura
+                OutputStream os = socketClient.getOutputStream(); //Abre flujo de escritura
 
-                System.out.println("SERVER:\nLeyendo mensaje del cliente...");
-                System.out.println("Mensaje cliente: " + is.read());
+                System.out.println("SERVER:\nLeyendo mensaje del cliente...\n");
+                numCliente = is.read();
+                //Muestra el número del cliente y llama a la funcion esPrimo para comprobar si lo es o no, si devuleve true escribe " y es primo", si no, " y no es primo"
+                System.out.println("MENSAJE DEL CLIENTE\nEl cliente ha enviado el número " + numCliente + ((esPrimo(numCliente))?" y es primo":" y no es primo"));
 
-                System.out.println("SERVER:\nEnviando mensaje al cliente...");
+                System.out.println("SERVER:\nEnviando mensaje al cliente...\n");
                 os.write(7);
 
-                System.out.println("SERVER:\nCerrando flujos de E/S...");
-                is.close();
-                os.close();
+                System.out.println("SERVER:\nCerrando flujos de E/S...\n");
+                is.close(); //Cierra flujo de lectura
+                os.close(); //Cierra flujo de escritura
 
                 socketClient.close();
             }
@@ -37,5 +41,14 @@ public class Servidor {
             System.out.println("Error al crear el socket");
             e.printStackTrace();
         }
+    }
+
+    public static boolean esPrimo(int numero) {
+        boolean primo = false;
+        for (int x = 2; x < numero / 2 && !primo; x++) {
+            primo = (numero % x != 0);
+        }
+        primo = !(numero == 0 || numero == 1);
+        return primo;
     }
 }
