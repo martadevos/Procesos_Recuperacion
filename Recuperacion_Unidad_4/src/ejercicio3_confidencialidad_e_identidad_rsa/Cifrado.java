bfaa623c2bb241b022d3d95004f17d899a3705ef
mdevos <martadevos@iesnervion.es>
2023-06-13 11:02:35 +0200

    Error en descifrado ejercicio3

--- Recuperacion_Unidad_4/src/ejercicio3_confidencialidad_e_identidad_rsa/Cifrado.java
+++ Recuperacion_Unidad_4/src/ejercicio3_confidencialidad_e_identidad_rsa/Cifrado.java
@@ -1,4 +1,81 @@
 package ejercicio3_confidencialidad_e_identidad_rsa;
 
+import java.io.ByteArrayOutputStream;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
 public class Cifrado {
+    public static String cifrar(String mensajeCifrar, String rutaFicheroClavePublica, String rutaFicheroClavePrivada) {
+        String cadenaCifrada = "";
+        try {
+            // Ciframos con la clave pública
+            PublicKey clavePublica = KeysManager.getClavePublica(rutaFicheroClavePublica);
+            PrivateKey clavePrivada = KeysManager.getClavePrivada(rutaFicheroClavePrivada);
+            byte[] mensaje = mensajeCifrar.getBytes(StandardCharsets.UTF_8);
+
+            Cipher cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.ENCRYPT_MODE, clavePrivada);
+
+            // Se cifra el mensaje
+            byte[] mensajeCifrado1 = cipher.doFinal(mensaje);
+
+            // Se cifra el mensaje
+            byte[] mensajeCifrado2 = cifrarContenido(mensajeCifrado1, clavePublica);
+
+            // Lo imprimimos por pantalla en Base64
+            cadenaCifrada = Base64.getEncoder().encodeToString(mensajeCifrado1);
+
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println("El algoritmo seleccionado no existe");
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            System.err.println("No existe el padding seleccionado");
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            System.err.println("La clave introducida no es válida");
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            System.err.println("El tamaño del bloque utilizado no es correcto");
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("El padding utilizado es erróneo");
+            e.printStackTrace();
+        }
+        return cadenaCifrada;
+    }
+
+    public static byte[] cifrarContenido(byte[] contenido, Key clave) throws Exception {
+        // Crear objeto Cipher
+        Cipher cifrador = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+
+        // Inicializar cifrador en modo cifrado con la clave proporcionada
+        cifrador.init(Cipher.ENCRYPT_MODE, clave);
+
+        // Calcular tamaño del bloque
+        int tamanoBloque = (((RSAPublicKey)clave).getModulus().bitLength() + 7) / 8 - 11;
+
+        // Inicializar buffer de salida
+        ByteArrayOutputStream bufferSalida = new ByteArrayOutputStream();
+
+        // Cifrar el contenido en bloques
+        int offset = 0;
+        while (offset < contenido.length) {
+            int tamanoBloqueActual = Math.min(tamanoBloque, contenido.length - offset);
+            byte[] bloqueCifrado = cifrador.doFinal(contenido, offset, tamanoBloqueActual);
+            bufferSalida.write(bloqueCifrado);
+            offset += tamanoBloqueActual;
+        }
+
+        // Devolver contenido cifrado completo
+        return bufferSalida.toByteArray();
+    }
 }
--- Recuperacion_Unidad_4/src/ejercicio3_confidencialidad_e_identidad_rsa/Main.java
+++ Recuperacion_Unidad_4/src/ejercicio3_confidencialidad_e_identidad_rsa/Main.java
@@ -1,4 +1,39 @@
 package ejercicio3_confidencialidad_e_identidad_rsa;
 
+import java.security.KeyPair;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        List<String> infoFichero, infoCifrada, infoDescifrada;
+        System.out.println("Se van a crear las claves del emisor");
+        KeysManager.guardarClaves(KeysManager.generarClaves(), "src\\ejercicio3_confidencialidad_e_identidad_rsa\\ficheroClavePublicaEmisor.key", "src\\ejercicio3_confidencialidad_e_identidad_rsa\\ficheroClavePrivadaEmisor.key");
+
+        System.out.println("Se van a crear las claves del destinatario");
+        KeysManager.guardarClaves(KeysManager.generarClaves(), "src\\ejercicio3_confidencialidad_e_identidad_rsa\\ficheroClavePublicaDestinatario.key", "src\\ejercicio3_confidencialidad_e_identidad_rsa\\ficheroClavePrivadaDestinatario.key");
+
+        System.out.println("Se va a leer el fichero");
+        infoFichero = Utilidades.leeFichero("src\\ejercicio3_confidencialidad_e_identidad_rsa\\ficheroCifrar.txt");
+        System.out.println("Se va a cifrar el fichero");
+        infoCifrada = new ArrayList<>();
+        for (String info : infoFichero) {
+            infoCifrada.add(Cifrado.cifrar(info, "src\\ejercicio3_confidencialidad_e_identidad_rsa\\ficheroClavePublicaDestinatario.key", "src\\ejercicio3_confidencialidad_e_identidad_rsa\\ficheroClavePrivadaEmisor.key"));
+        }
+        for (String info : infoCifrada) {
+            Utilidades.escribeDatosEnFichero(info, "src\\ejercicio3_confidencialidad_e_identidad_rsa\\ficheroInfoCifrada.txt");
+        }
+
+        System.out.println("Se va a leer el fichero cifrado");
+        infoCifrada = Utilidades.leeFichero("src\\ejercicio3_confidencialidad_e_identidad_rsa\\ficheroInfoCifrada.txt");
+        System.out.println("Se va a descifrar el fichero");
+        infoDescifrada = new ArrayList<>();
+        for (String info : infoCifrada) {
+            infoDescifrada.add(Descifrado.descifraCadena(info, "src\\ejercicio3_confidencialidad_e_identidad_rsa\\ficheroClavePrivadaDestinatario.key"));
+        }
+        System.out.println("Se va a escribir el fichero descifrado");
+        for (String info : infoDescifrada) {
+            Utilidades.escribeDatosEnFichero(info, "src\\ejercicio3_confidencialidad_e_identidad_rsa\\ficheroInfoDescifrada.txt");
+        }
+    }
 }
