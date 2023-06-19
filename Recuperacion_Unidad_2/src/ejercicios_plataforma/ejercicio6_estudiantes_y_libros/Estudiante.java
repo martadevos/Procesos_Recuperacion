2ba5a1696469e667d46a2dfc5951c21830e41c96
martadevos <marta.devos@iesnervion.es>
2023-06-19 18:59:26 +0200

    19/06/2023

--- Recuperacion_Unidad_2/src/ejercicios_plataforma/ejercicio6_estudiantes_y_libros/Estudiante.java
+++ Recuperacion_Unidad_2/src/ejercicios_plataforma/ejercicio6_estudiantes_y_libros/Estudiante.java
@@ -1,25 +1,25 @@
 package ejercicios_plataforma.ejercicio6_estudiantes_y_libros;
 
 public class Estudiante extends Thread {
-    public static final Libro[] libros = {new Libro(1), new Libro(2), new Libro(3), new Libro(4), new Libro(5), new Libro(6), new Libro(7), new Libro(8), new Libro(9)};
 
+    public static final boolean[] libro = {false, false, false, false, false, false, false, false, false};
     @Override
     public void run() {
         int[] librosLeer = new int[]{(int) (Math.random() * 8), (int) (Math.random() * 8)};
         try {
-            synchronized (libros) {
-                while (libros[librosLeer[0]].isOcupado() || libros[librosLeer[1]].isOcupado()) {
-                    libros.wait();
+            synchronized (libro) {
+                while (libro[librosLeer[0]] || libro[librosLeer[1]]) {
+                    libro.wait();
                 }
-                libros[librosLeer[0] - 1].setOcupado(true);
-                libros[librosLeer[1] - 1].setOcupado(true);
-                System.out.println(currentThread().getName() + ":\nEstá leyendo los libros\n" + libros[librosLeer[0]].getNumLibro() + " y " + libros[librosLeer[1]].getNumLibro() + "\n");
+                libro[librosLeer[0]] = true;
+                libro[librosLeer[1]] = true;
+                System.out.println(currentThread().getName() + ":\nEstá leyendo los libros\n" + (librosLeer[0] + 1) + " y " + (librosLeer[1] + 1) + "\n");
             }
             Thread.sleep((long) (Math.random() * 5 + 2));
-            synchronized (libros) {
-                libros[librosLeer[0] - 1].setOcupado(false);
-                libros[librosLeer[1] - 1].setOcupado(false);
-                libros.notifyAll();
+            synchronized (libro) {
+                libro[librosLeer[0]] = false;
+                libro[librosLeer[1]] = false;
+                libro.notifyAll();
             }
             System.out.println(currentThread().getName() + ":\nHa soltado los libros\n");
         } catch (InterruptedException e) {
